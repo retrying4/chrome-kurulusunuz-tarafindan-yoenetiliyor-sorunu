849a8a1ef335e5670a4483a5b09d6eda14b2bfdd
retrying4 <59968072+retrying4@users.noreply.github.com>
2021-05-08 16:45:08 +0300

    Create run_administrator.bat

---
+++ run_administrator.bat
@@ -0,0 +1,6 @@
+reg delete HKEY_CURRENT_USER\Software\Google\Chrome  /va
+reg delete HKEY_CURRENT_USER\Software\Policies\Google\Chrome  /va
+reg delete HKEY_LOCAL_MACHINE\Software\Google\Chrome  /va
+reg delete HKEY_LOCAL_MACHINE\Software\Policies\Google\Chrome  /va
+reg delete HKEY_LOCAL_MACHINE\Software\Policies\Google\Update  /va
+reg delete HKEY_LOCAL_MACHINE\Software\WOW6432Node\Google\Enrollment  /va
